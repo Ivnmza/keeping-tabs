b8cd0bf3343d8a1af1befa7b5628b1623943b8cf
Ivan Meza <ivn.mza@gmail.com>
2016-04-01 15:49:17 -0700

    WIP: Fixing CE request to server for build scripts

---
+++ test/appspec.js
@@ -0,0 +1,10 @@
+// Testing Specifications
+var expect = require('chai').expect;
+var fs = require('fs');
+var server = require("../app/server/index.js");
+var init = require("../app/chrome/init.js");
+var handler = require("../server/requestHandler.js ");
+var path   = require('path');
+
+// Server
+describe()
